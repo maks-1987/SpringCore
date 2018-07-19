62ced1e1faf7c28385ce522709e9c83958183de3
maks-1987 <max.moderation@gmail.com>
2018-07-19 09:59:07 +0300

    Tkach 3 lect

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Приложение будет проверять message и подменять на полноеимя + печатать
  * его на консоль.
@@ -7,12 +10,20 @@ public class App {
     Client client;
     ConsoleEventLogger eventLogger;
 
-    public App(Client client, ConsoleEventLogger eventLogger) {
+    App(Client client, ConsoleEventLogger eventLogger) {
         this.client = client;
         this.eventLogger = eventLogger;
     }
 
     public static void main(String[] args) {
+        // указываем где искать настройки spring
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
+
+        // вызвать у бина app метод logEvent
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
 
         //app.client = new Client("1", "Jon Smith"); // не будем создавать Клиента
         //app.eventLogger = new ConsoleEventLogger(); // и КонсольЭвентЛоггер
