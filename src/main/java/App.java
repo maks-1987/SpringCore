8b99c0b4f4a7192362e9f2b6d2f41b708e6f02ba
maks-1987 <max.moderation@gmail.com>
2018-08-19 10:42:15 +0300

    11 lect Error creating bean with name 'statisticsAspect'

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import Beans.Event;
 import Beans.EventType;
 import Loggers.EventLogger;
 
+import aspects.StatisticsAspect;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -18,21 +19,25 @@ public class App {
     private EventLogger defaultLogger; // объект интерфейса ЭвентЛоггер
     private Map<EventType, EventLogger> loggers;
     private String startupMessage;
+    private StatisticsAspect statisticsAspect;
 
     public static void main(String[] args) {
-        // указываем где искать настройки spring
-        ConfigurableApplicationContext ctx = new
-                ClassPathXmlApplicationContext("spring.xml");
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App) ctx.getBean("app");
 
         System.out.println(app.startupMessage);
 
         Client client = ctx.getBean(Client.class);
-        System.out.println("Class says: " + client.getGreeting());
+        System.out.println("Client says: " + client.getGreeting());
 
         Event event = ctx.getBean(Event.class);
-        // вызвать у бина app метод logEvent
         app.logEvent(EventType.INFO, event, "Some event for 1");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(EventType.INFO, event, "One more event for 1");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(EventType.INFO, event, "And one more event for 1");
 
         event = ctx.getBean(Event.class);
         app.logEvent(EventType.ERROR, event, "Some event for 2");
@@ -40,23 +45,18 @@ public class App {
         event = ctx.getBean(Event.class);
         app.logEvent(null, event, "Some event for 3");
 
+        app.outputLoggingCounter();
+
         ctx.close();
-        //app.client = new Beans.Client("1", "Jon Smith"); // не будем создавать Клиента
-        //app.eventLogger = new Loggers.ConsoleEventLogger(); // и КонсольЭвентЛоггер
-        //app.logEvent("Beans.Event for User_1");
     }
 
-    public App(Client client, EventLogger eventLogger, Map<EventType,
-                EventLogger> loggers) {
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
         super();
         this.client = client;
         this.defaultLogger = eventLogger;
         this.loggers = loggers;
-
     }
 
-    // принимает строку и обращается к ЭвентЛоггеру, чтобы он уже ею
-    // занимался. Метод replaceAll из библ Джава заменит ИД на ИМЯ
     private void logEvent(EventType eventType, Event event, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
@@ -69,8 +69,22 @@ public class App {
         logger.logEvent(event);
     }
 
+    private void outputLoggingCounter() {
+        if (statisticsAspect != null) {
+            System.out.println("Loggers statistics. Number of calls: ");
+            for (Map.Entry<Class<?>, Integer>
+                    entry: statisticsAspect.getCounter().entrySet()) {
+                System.out.println("    " + entry.getKey().getSimpleName() + ": " + entry.getValue());
+            }
+        }
+    }
+
     public void setStartupMessage(String startupMessage) {
         this.startupMessage = startupMessage;
+    }
+
+    public void setStatisticsAspect(StatisticsAspect statisticsAspect) {
+        this.statisticsAspect = statisticsAspect;
     }
 
     public EventLogger getDefaultLogger() {
@@ -78,6 +92,21 @@ public class App {
     }
 
 }/*
+org.springframework.beans.factory.BeanCreationException: Error creating
+bean with name 'statisticsAspect' defined in class path resource
+[aspects.xml]: BeanPostProcessor before instantiation of bean failed;
+nested exception is org.springframework.beans.factory.BeanCreationException:
+Error creating bean with name 'org.springframework.aop.aspectj.AspectJPointcutAdvisor#0':
+Cannot create inner bean '(inner bean)#7dfd3c81' of type
+[org.springframework.aop.aspectj.AspectJAroundAdvice] while setting constructor
+argument; nested exception is org.springframework.beans.factory.BeanCreationException:
+Error creating bean with name '(inner bean)#7dfd3c81': Cannot resolve reference
+to bean 'consoleLogging' while setting constructor argument; nested exception
+is org.springframework.beans.factory.BeanCreationException: Error creating
+bean with name 'consoleLogging': Instantiation of bean failed; nested
+exception is java.lang.NoClassDefFoundError:
+org/aspectj/weaver/tools/PointcutDesignatorHandler
+
     Some event for John Smith я из логИвента
     Some event for 2 я из логИвента
 */
